3e00d219a1e011ac34700db9662cea1f08936259
alynek <alyne0x67@gmail.com>
2022-06-24 12:23:13 -0300

    Create King and Rook classes

---
+++ Chess/ChessGame/King.cs
@@ -0,0 +1,14 @@
+﻿using Chess.BoardGame;
+
+namespace Chess.ChessGame
+{
+    public class King : Piece
+    {
+        public King(Color color, Board board) : base(color, board){}
+
+        public override string ToString()
+        {
+            return ColorIsWhite(Color) ? " \u2654 " : " \u265a ";
+        }
+    }
+}
---
+++ Chess/ChessGame/Rook.cs
@@ -0,0 +1,14 @@
+﻿using Chess.BoardGame;
+
+namespace Chess.ChessGame
+{
+    public class Rook : Piece
+    {
+        public Rook(Color color, Board board) : base(color, board){}
+
+        public override string ToString()
+        {
+            return ColorIsWhite(Color) ? " \u2656 " : "\u265c";
+        }
+    }
+}
