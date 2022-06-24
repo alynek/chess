e31da97664933a37fbcd3e1453d70249ad6e0cfe
alynek <alyne0x67@gmail.com>
2022-06-24 12:24:43 -0300

    Create Screen class responsible for displaying the game screen

---
+++ Chess/Screen.cs
@@ -0,0 +1,26 @@
+﻿using Chess.BoardGame;
+
+namespace Chess
+{
+    public class Screen
+    {
+        public static void SetBackgroundColorConsole() => Console.BackgroundColor = ConsoleColor.Gray;
+        public static void SetFontConsole() => Console.OutputEncoding = System.Text.Encoding.UTF8;
+        public static void PrintBoard(Board board)
+        {
+
+            for (int i = 0; i < board.Rows; i++)
+            {
+                for(int j = 0; j < board.Columns; j++)
+                {
+                    ConsoleColor aux = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(board._pieces[i, j]?.ToString() ?? " - ");
+                    Console.ForegroundColor = aux;
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
