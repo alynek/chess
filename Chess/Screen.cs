0990fcaca0b4898fab19b66a26142c8e47d40e06
alynek <alyne0x67@gmail.com>
2022-06-24 16:22:50 -0300

    Remove BackgroundColor

--- Chess/Screen.cs
+++ Chess/Screen.cs
@@ -4,7 +4,6 @@ namespace Chess
 {
     public class Screen
     {
-        public static void SetBackgroundColorForGame() => Console.BackgroundColor = ConsoleColor.Gray;
         public static void SetBackgroundColorForError() => Console.BackgroundColor = ConsoleColor.Black;
         public static void SetFontConsole() => Console.OutputEncoding = System.Text.Encoding.UTF8;
         public static void PrintBoard(Board board)
@@ -14,10 +13,7 @@ namespace Chess
             {
                 for(int j = 0; j < board.Columns; j++)
                 {
-                    ConsoleColor aux = Console.ForegroundColor;
-                    Console.ForegroundColor = ConsoleColor.Black;
                     Console.Write(board._pieces[i, j]?.ToString() ?? " - ");
-                    Console.ForegroundColor = aux;
                     
                 }
                 Console.WriteLine();
