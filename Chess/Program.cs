c9c2cf3223d215986d75185aa43e557942c76e20
alynek <alyne0x67@gmail.com>
2022-06-24 12:27:03 -0300

    Instantiate a board object

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Chess
+﻿using Chess.BoardGame;
+using Chess.ChessGame;
+
+namespace Chess
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            Screen.SetBackgroundColorConsole();
+            Screen.SetFontConsole();
 
+            Board board = new Board();
+
+            board.PutPiece(new King(Color.BLACK, board), new Position(1, 2));
+            board.PutPiece(new Rook(Color.WHITE, board), new Position(2, 4));
+
+            Screen.PrintBoard(board);
+            Console.ReadLine();
         }
     }
 }
