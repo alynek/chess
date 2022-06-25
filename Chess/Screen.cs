e636eadfb70d29a1555d0110a2c96430f8ef53b4
alynek <alyne0x67@gmail.com>
2022-06-24 23:11:18 -0300

    Add algebraic notation to board

--- Chess/Screen.cs
+++ Chess/Screen.cs
@@ -5,18 +5,23 @@ namespace Chess
     public class Screen
     {
         public static void SetFontConsole() => Console.OutputEncoding = System.Text.Encoding.UTF8;
+        private static void PrintColumnNumbers(int rows, int i) => Console.Write($"{rows - i} ");
+        private static void PrintRowLetters() => Console.WriteLine("   a  b  c  d  e  f  g  h");
+
         public static void PrintBoard(Board board)
         {
 
             for (int i = 0; i < board.Rows; i++)
             {
+                PrintColumnNumbers(board.Rows, i);
+
                 for(int j = 0; j < board.Columns; j++)
                 {
                     Console.Write(board._pieces[i, j]?.ToString() ?? " - ");
-                    
                 }
                 Console.WriteLine();
             }
+            PrintRowLetters();
         }
     }
 }
