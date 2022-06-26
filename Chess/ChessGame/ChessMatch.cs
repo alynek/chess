b44012a4939026b74554fe9c6c27287b2ae450b2
alynek <alyne0x67@gmail.com>
2022-06-26 19:26:47 -0300

    Add finished property

---
+++ Chess/ChessGame/ChessMatch.cs
@@ -0,0 +1,37 @@
+﻿using Chess.BoardGame;
+
+namespace Chess.ChessGame
+{
+    public class ChessMatch
+    {
+        public Board board { get; private set; }
+        public bool finished { get; private set; }
+        private int _turn;
+        private Color _currentPlayer;
+
+        public ChessMatch()
+        {
+            board = new Board();
+            _turn = 1;
+            _currentPlayer = Color.WHITE;
+            finished = false;
+            PutPieces();
+        }
+
+        private void PutPieces()
+        {
+            board.PutPiece(new King(Color.BLACK, board), new ChessPosition('b', 2).GetPositionByInput());
+            board.PutPiece(new King(Color.WHITE, board), new ChessPosition('c', 1).GetPositionByInput());
+            board.PutPiece(new Rook(Color.WHITE, board), new ChessPosition('h', 1).GetPositionByInput());
+        }
+
+        public void MakeMove(Position source, Position target)
+        {
+            Piece piece = board.RemovePiece(source);
+            piece.IncrementMoveQuantity();
+
+            Piece capturedPiece = board.RemovePiece(target);
+            board.PutPiece(piece, target);
+        }
+    }
+}
